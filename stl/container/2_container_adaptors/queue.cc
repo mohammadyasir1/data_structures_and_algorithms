da5dc963fce3258ff9335ff385bccf83e29fd0aa
Mohammad Yasir <68849840+ycisir@users.noreply.github.com>
2023-06-24 17:07:22 +0530

    Add files via upload

---
+++ stl/container/2_container_adaptors/queue.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	queue<string> q;
+	q.push("gnu");
+	q.push("linux");
+	q.push("xfce");
+
+	cout<<"top element in stack is: "<<q.front()<<endl; 
+	
+	cout<<"size of stack before pop->: "<<q.size()<<endl; 
+
+	q.pop();
+	cout<<"top element in stack is: "<<q.front()<<endl; 
+
+	cout<<"size of stack after pop->: "<<q.size()<<endl; 
+
+	return 0;
+}
