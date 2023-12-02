3be415af18eac3fa57434d589574a635805db922
Mohammad Yasir <68849840+ycisir@users.noreply.github.com>
2023-12-02 15:31:36 +0530

    Update 1_print_all_divisors_of_a_number.cc

--- 3_function_problems/1_print_all_divisors_of_a_number.cc
+++ 3_function_problems/1_print_all_divisors_of_a_number.cc
@@ -1,1 +1,38 @@
-test
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> printDivisors(int n){
+        vector<int>v;
+
+        for(int i=1; i*i<=n; i++) {
+                if(n%i==0) {
+                        v.push_back(i);
+                        if(i!=n/i){
+                                v.push_back(n/i);
+                        }
+                }
+        }
+
+        sort(v.begin(), v.end());
+        return v;
+}
+
+void print(vector<int>arr, int size) {
+        for(int i=0; i<size; i++) {
+                cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+}
+
+int main(){
+        int n;
+        cout<<"Enter the number: ";
+        cin>>n;
+        vector<int> temp = printDivisors(n);
+        int size = temp.size();
+        print(temp, size);
+
+        return 0;
+}
