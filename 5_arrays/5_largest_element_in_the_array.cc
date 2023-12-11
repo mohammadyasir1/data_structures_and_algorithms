c0620a6f36c36f8b964259b33738dbe3bbcd3e7c
Mohammad Yasir <68849840+ycisir@users.noreply.github.com>
2023-12-11 20:44:39 +0530

    Add files via upload

---
+++ 5_arrays/5_largest_element_in_the_array.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int largestElement(vector<int> &arr, int n) {
+	int largest_element = arr[0];
+
+	for(int i=0; i<n; i++) {
+		if(arr[i] > largest_element)
+			largest_element = arr[i];
+	}
+	return largest_element;
+}
+
+int main() {
+	int n;
+	vector<int> arr = {4,7,8,6,7,6};
+	n = 6;
+
+	cout<<"largest element in the array is "<<largestElement(arr, n)<<endl;
+	return 0;
+}
