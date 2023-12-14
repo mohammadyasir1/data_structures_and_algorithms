5c3d7b146f3e2a5f04cd20403da694c434abaecb
Mohammad Yasir <68849840+ycisir@users.noreply.github.com>
2023-12-14 19:11:30 +0530

    Add files via upload

---
+++ 7_linked_list/5_middle_of_linked_list.cc
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node{
+public:
+	int data;
+	node* next;
+
+	node(int data){
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+node* insertion(node* &head, int position, int data){
+	node* newNode = new node(data);
+
+	if(position == 1){
+		newNode->next = head;
+		head = newNode;
+		return head;
+	}
+
+	node* temp = head;
+	int pos = 2;
+
+	while(pos < position){
+		pos++;
+		temp = temp->next;
+	}
+
+	newNode->next = temp->next;
+	temp->next = newNode;
+
+	return head;
+
+
+}
+
+int len(node* &head){
+	node* ptr = head;
+	int length = 0;
+	while(ptr != NULL){
+		length++;
+		ptr = ptr->next;
+	}
+	return length;
+}
+
+node* findMiddle(node* &head){
+	
+	// if(head == NULL)
+	// 	return NULL;
+
+	int mid = len(head)/2;
+
+	for(int i=1; i<=mid; i++){
+		head = head->next;
+	}
+
+	return head;
+
+
+	//two pointer approach
+	// if(head == NULL)
+	// 	return head;
+
+	// if(head->next == NULL)
+	// 	return head;
+
+	// node* fast = head;
+	// node* slow = head;
+
+	// while(fast != NULL && slow != NULL){
+	// 	fast = fast->next->next;
+	// 	slow = slow->next;
+	// }
+
+	// return slow;
+
+}
+
+void print(node* &head){
+	node* temp = head;
+	while(temp != NULL){
+		cout<<temp->data<<" ";
+		temp = temp->next;
+	}
+	cout<<endl;
+}
+
+int main(){
+	node* n1 = new node(1);
+	node* head = n1;
+	insertion(head, 2, 2);
+	insertion(head, 3, 3);
+	insertion(head, 4, 4);
+	insertion(head, 5, 5);
+	insertion(head, 6, 6);
+	print(head);
+	cout<<"length = "<<len(head)<<endl;
+	findMiddle(head);
+	print(head);
+
+	return 0;
+}
