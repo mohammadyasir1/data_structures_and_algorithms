2961888961f286ba3f89f729d4737ec25a4bcaa2
Mohammad Yasir <68849840+ycisir@users.noreply.github.com>
2023-12-14 13:20:59 +0530

    Add files via upload

---
+++ 7_linked_list/4.1_linked_list_insertion.cc
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node{
+public:
+	int data;//data value
+	node* next;//pointer that store next data value
+
+	node(int data){
+		this->data = data;//given value put in data
+
+		this->next = NULL;//initially pointing null
+	}
+};
+
+// function for inserting data in list at any position
+node* insertion(node* &head, int position, int data){
+	// creating new node
+	node* newNode = new node(data);
+
+	// insertion at head
+	if(position == 1){
+		newNode->next = head;
+		head = newNode;
+		return head;
+	}
+
+	// current position index
+	int pos = 2;
+
+	// temporary pointer to the head node
+	node* temp = head;
+
+	while(pos < position){
+		pos++;
+		temp = temp->next;
+	}
+
+	// now temp is pointing to the previos node
+	// of the node to be inserted
+
+
+	// next pointer of the inserted node to the next node
+	// to be inserted
+	newNode->next = temp->next;
+
+
+	// next pointer of the temp node to the node
+	// to be inserted
+	temp->next = newNode; 
+
+}
+
+void print(node* head){
+	node* temp = head;
+
+	while(temp != NULL){
+		cout<<temp->data<<" ";
+		temp = temp->next;
+	}
+	cout<<endl;
+}
+
+
+int main(){
+	node* n1 = new node(78);
+
+	node* head = n1;
+	insertion(head, 2, 56);
+	insertion(head, 3, 4);
+
+
+
+	// insertion at start
+	insertion(head, 1, 19);
+
+	//insertion anywhere in list
+	insertion(head, 2, 47);
+
+	//insertion at last
+	insertion(head, 6, 99);
+
+	print(head);
+	return 0;
+}
