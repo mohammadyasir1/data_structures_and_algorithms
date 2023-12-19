9801d4581021f78c723a11719f7ce56e8702e6bc
Mohammad Yasir <68849840+ycisir@users.noreply.github.com>
2023-12-19 12:59:03 +0530

    Update 1_redundant_brackets.cc

--- 8_stack_and_queues/1_redundant_brackets.cc
+++ 8_stack_and_queues/1_redundant_brackets.cc
@@ -1,7 +1,62 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+bool findRedundantBrackets(string& s){
+	stack<char> st;
 
-return 0;
+	int len = s.size();
+
+	for(int i=0; i<len; i++){
+
+		char ch = s[i];
+
+		if(ch == '(' || ch == '+' || ch == '-' || ch == '*' || ch == '/'){
+
+			st.push(ch);
+
+		} else{
+
+			if(ch == ')'){
+
+				bool flag = true;
+
+				while(st.top() != '('){
+
+					char top = st.top();
+
+					//check operators in stacks between (this)
+					// if exist means expression dont have redundant brackets
+					if(top == '+' || top == '-' || top == '*' || top == '/'){
+						flag = false;
+					}
+
+					st.pop();
+
+				}
+
+				if(flag == true){//there are redundant brackets
+					return true;
+				}
+
+				st.pop();
+			}
+		}	
+	}
+	return false;
+}
+
+int main(){
+
+	string s;
+	cout<<"enter the expression: ";
+	cin>>s;
+
+	bool ans = findRedundantBrackets(s);
+	// cout<<ans<<endl;
+	if(ans)
+		cout<<"Yes"<<endl;
+	else
+		cout<<"No"<<endl;
+
+	return 0;
 }
