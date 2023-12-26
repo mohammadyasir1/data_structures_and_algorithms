c296d76f8def051fadae2c467e66824b042cba5b
Mohammad Yasir <68849840+ycisir@users.noreply.github.com>
2023-12-26 18:38:26 +0530

    Add files via upload

--- 9_recursion_and_backtracking/11_tug_of_war.cc
+++ 9_recursion_and_backtracking/11_tug_of_war.cc
@@ -33,6 +33,45 @@ int tugOfWar(vector<int> &arr, int n) {
 
 
 
+
+// int tugOfWarHelper(int i, int subsetSum, int cnt, int totalSum, vector<int> &arr, int n)
+// {
+//     // base case
+//     if (i == n || cnt >= (n / 2))
+//     {
+//         if (cnt == (n / 2))
+//         {
+//             return abs(totalSum - 2 * subsetSum);
+//         }
+//         return INT_MAX;
+//     }
+//     int ans = INT_MAX;
+
+//     // don't take ith student
+//     ans = min(ans, tugOfWarHelper(i + 1, subsetSum, cnt, totalSum, arr, n));
+
+//     // take ith student
+//     ans = min(ans, tugOfWarHelper(i + 1, subsetSum + arr[i], cnt + 1, totalSum, arr, n));
+
+//     return ans;
+// }
+
+// int tugOfWar(vector<int> &arr, int n)
+// {
+//     int totalSum = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         totalSum += arr[i];
+//     }
+
+//     // call the recursive function
+//     int ans = tugOfWarHelper(0, 0, 0, totalSum, arr, n);
+
+//     return ans;
+// }
+
+
+
 int main() {
 
 	int n;
