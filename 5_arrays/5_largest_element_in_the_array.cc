bb0dfb77b5352c562bec39af6dd2f10d10c0db1d
Mohammad Yasir <68849840+ycisir@users.noreply.github.com>
2024-01-02 18:43:20 +0530

    Update 5_largest_element_in_the_array.cc

--- 5_arrays/5_largest_element_in_the_array.cc
+++ 5_arrays/5_largest_element_in_the_array.cc
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
+
+// int largestElement(vector<int> &arr, int n) {
+//     sort(arr.begin(), arr.end());
+//     return arr[n-1];
+// }
+
 
 int largestElement(vector<int> &arr, int n) {
 	int largest_element = arr[0];
